3662284bb9bd138df41546a4b5d586bd29bd6886
PorametKeawubol <poramet.contact@gmail.com>
2025-02-28 22:12:34 +0700

    update database connection strings and guest user credentials in environment files

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -9,9 +9,7 @@ export const config = (): ConfigProps => ({
   },
   mongodb: {
     database: {
-      connectionString:
-        process.env.MONGODB_CONNECTION_STRING ||
-        'mongodb://localhost:27017/leetcode',
+      connectionString: process.env.MONGODB_CONNECTION_STRING,
       databaseName: process.env.NODE_ENV || 'local',
     },
   },
