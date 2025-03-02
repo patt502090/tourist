3c0aaad1ee28a3d8f8ac7d1b029cc3d3f3a6595f
PorametKeawubol <poramet.contact@gmail.com>
2025-03-02 15:45:35 +0700

    refactor config to remove default values for port and database name

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -2,7 +2,7 @@ import { ConfigProps } from 'src/interfaces/config.interface';
 import * as dotenv from 'dotenv';
 dotenv.config();
 export const config = (): ConfigProps => ({
-  port: parseInt(process.env.PORT, 10) || 8080,
+  port: parseInt(process.env.PORT, 10),
   api: {
     apiUrl: process.env.API_URL,
     httpTimeout: 1000,
@@ -10,7 +10,7 @@ export const config = (): ConfigProps => ({
   mongodb: {
     database: {
       connectionString: process.env.MONGODB_CONNECTION_STRING,
-      databaseName: process.env.NODE_ENV || 'local',
+      databaseName: process.env.NODE_ENV,
     },
   },
 });
